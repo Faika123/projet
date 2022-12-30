14c0cb0eff1decd73db03ae4ce72e9b36fa26943
Faika123 <faikabenzaara@gmail.com>
2022-12-30 12:01:02 +0100

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header', component: HeaderComponent},
+  {path: 'auth', component: AuthentificationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudiantComponent,
+    HeaderComponent,
+    AuthentificationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
